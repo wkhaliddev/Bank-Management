cca4c794f0e53354eb9e3a866ffa3678998bad29
Salman Saleem <salmansaleem49@gmail.com>
2019-10-17 20:30:00 +0500

    Improvement in the readablitity of the code.

--- DSAproject/Node.cpp
+++ DSAproject/Node.cpp
@@ -3,11 +3,12 @@
 
 Node::Node()
 {
+	//This is a bidirectional node which requires a pointer to both the next and previous node.
 	next = nullptr;
 	pre = nullptr;
 	data = 0;
 }
-Node::Node(int d)
+Node::Node(int d)//Parameterized Constructor
 {
 	next = nullptr;
 	pre = nullptr;
